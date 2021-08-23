83d9cafce5abcaaf92da58eb80399776f9b1afd6
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-08-24 00:15:31 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -851,6 +851,14 @@ except KeyError:
     logging.warning('SERVER_PORT not provided!')
     SERVER_PORT = None
 
+try:
+    BASE_URL_OF_BOT = getConfig(f'https://{HEROKU_APP_NAME}.herokuapp.com')
+    if len(BASE_URL_OF_BOT ) == 0:
+        BASE_URL_OF_BOT = None
+except KeyError:
+    logging.warning('BASE_URL_OF_BOT not provided!')
+    BASE_URL_OF_BOT = BASE_URL_OF_BOT 
+
 updater = tg.Updater(token=BOT_TOKEN)
 bot = updater.bot
 dispatcher = updater.dispatcher
